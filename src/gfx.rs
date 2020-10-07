0b720a2017442e2394a03e946c88d52e4fc46a7d
Antonio Caggiano <info@antoniocaggiano.eu>
2020-10-07 22:04:00 +0000

    Draw two triangles
    Create vertex and fragment shader, and link them together into a shader
    program. Then create a mesh with two triangles from a list of vertices
    and indices. Use the shader program and bind the mesh before drawing it.

---
+++ src/gfx.rs
@@ -0,0 +1,179 @@
+use std::ffi::CString;
+
+pub struct Shader {
+    handle: u32,
+}
+
+impl Shader {
+    pub fn new(shader_type: gl::types::GLenum, src: &str) -> Option<Shader> {
+        unsafe {
+            let handle = gl::CreateShader(shader_type);
+            let c_src = CString::new(src.as_bytes()).unwrap();
+            gl::ShaderSource(handle, 1, &c_src.as_ptr(), std::ptr::null());
+            gl::CompileShader(handle);
+
+            // Check error compiling shader
+            let mut success = gl::FALSE as gl::types::GLint;
+            let length = 512;
+            let mut log = Vec::with_capacity(length);
+            log.set_len(length as usize - 1);
+            gl::GetShaderiv(handle, gl::COMPILE_STATUS, &mut success);
+
+            if success != gl::TRUE as gl::types::GLint {
+                let mut ilen = length as i32;
+                gl::GetShaderInfoLog(
+                    handle,
+                    511,
+                    &mut ilen as *mut i32,
+                    log.as_mut_ptr() as *mut gl::types::GLchar,
+                );
+                log.set_len(ilen as usize);
+                let message = CString::from(log);
+                println!("Compilation failed: {}", message.to_str().unwrap());
+                None
+            } else {
+                Some(Shader { handle })
+            }
+        }
+    }
+}
+
+impl Drop for Shader {
+    fn drop(&mut self) {
+        unsafe { gl::DeleteShader(self.handle) };
+    }
+}
+
+pub struct ShaderProgram {
+    handle: u32,
+}
+
+impl ShaderProgram {
+    pub fn new(vert: Shader, frag: Shader) -> ShaderProgram {
+        unsafe {
+            let handle = gl::CreateProgram();
+            gl::AttachShader(handle, vert.handle);
+            gl::AttachShader(handle, frag.handle);
+            gl::LinkProgram(handle);
+            ShaderProgram { handle }
+        }
+    }
+
+    pub fn enable(&self) {
+        unsafe { gl::UseProgram(self.handle) };
+    }
+}
+
+struct Vbo {
+    handle: u32,
+}
+
+impl Vbo {
+    fn new() -> Vbo {
+        let mut handle = 0;
+        unsafe { gl::GenBuffers(1, &mut handle) };
+        Vbo { handle }
+    }
+
+    fn bind(&self) {
+        unsafe { gl::BindBuffer(gl::ARRAY_BUFFER, self.handle) };
+    }
+
+    fn upload(&self, vertices: &Vec<f32>) {
+        unsafe {
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (vertices.len() * std::mem::size_of::<f32>()) as isize,
+                vertices.as_ptr() as *const libc::c_void,
+                gl::STATIC_DRAW,
+            )
+        };
+    }
+}
+
+struct Ebo {
+    handle: u32,
+}
+
+impl Ebo {
+    fn new() -> Ebo {
+        let mut handle = 0;
+        unsafe { gl::GenBuffers(1, &mut handle) };
+        Ebo { handle }
+    }
+
+    fn bind(&self) {
+        unsafe { gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, self.handle) };
+    }
+
+    fn upload(&self, indices: &Vec<u32>) {
+        unsafe {
+            gl::BufferData(
+                gl::ELEMENT_ARRAY_BUFFER,
+                (indices.len() * std::mem::size_of::<u32>()) as isize,
+                indices.as_ptr() as *const libc::c_void,
+                gl::STATIC_DRAW,
+            )
+        };
+    }
+}
+
+struct Vao {
+    handle: u32,
+}
+
+impl Vao {
+    fn new() -> Vao {
+        let mut handle = 0;
+        unsafe { gl::GenVertexArrays(1, &mut handle) };
+        Vao { handle }
+    }
+
+    fn bind(&self) {
+        unsafe { gl::BindVertexArray(self.handle) };
+    }
+}
+
+pub struct MeshRes {
+    _vbo: Vbo,
+    _ebo: Ebo,
+    vao: Vao,
+}
+
+impl MeshRes {
+    pub fn new(vertices: &Vec<f32>, indices: &Vec<u32>) -> Self {
+        let vbo = Vbo::new();
+        let ebo = Ebo::new();
+        let vao = Vao::new();
+
+        vao.bind();
+        vbo.bind();
+        vbo.upload(&vertices);
+        ebo.bind();
+        ebo.upload(&indices);
+
+        // These should follow Vao, Vbo, Ebo
+        unsafe {
+            // Position
+            gl::VertexAttribPointer(
+                0,
+                3,
+                gl::FLOAT,
+                gl::FALSE,
+                3 * std::mem::size_of::<f32>() as i32,
+                0 as *const std::ffi::c_void,
+            );
+            gl::EnableVertexAttribArray(0);
+        }
+
+        Self {
+            _vbo: vbo,
+            _ebo: ebo,
+            vao,
+        }
+    }
+
+    pub fn bind(&self) {
+        self.vao.bind();
+    }
+}
