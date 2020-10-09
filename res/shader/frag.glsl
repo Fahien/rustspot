47bee7d72ffbc22861ccdd7cdf777a942eb730de
Antonio Caggiano <info@antoniocaggiano.eu>
2020-10-09 21:29:13 +0000

    Draw a texture
    Add a Texture class, load a png image, improve Vertex with texture
    coordinates attribute, and implement drop to release resources.

--- res/shader/frag.glsl
+++ res/shader/frag.glsl
@@ -3,7 +3,10 @@
 out mediump vec4 out_color;
 
 in mediump vec3 color;
+in mediump vec2 tex_coords;
+
+uniform sampler2D tex_sampler;
 
 void main() {
-    out_color = vec4(color, 1.0);
+    out_color = texture(tex_sampler, tex_coords);
 }
