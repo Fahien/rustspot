0198177f3677ba287016e2b3297ca528db1236d0
Antonio Caggiano <info@antoniocaggiano.eu>
2020-10-09 23:31:35 +0000

    Rotate a node
    Use a node to store a model matrix. Use nalgebra isometry to represent
    this transform matrix. Bind it as a uniform to obtain a rotating effect.

--- res/shader/vert.glsl
+++ res/shader/vert.glsl
@@ -4,11 +4,13 @@ layout (location = 0) in vec3 in_pos;
 layout (location = 1) in vec3 in_color;
 layout (location = 2) in vec2 in_tex_coords;
 
+layout (location = 0) uniform mat4 model;
+
 out vec3 color;
 out vec2 tex_coords;
 
 void main() {
     color = in_color;
     tex_coords = in_tex_coords;
-    gl_Position = vec4(in_pos, 1.0);
+    gl_Position = model * vec4(in_pos, 1.0);
 }
