9527b686c64eb6fea96a4d507bb86c623d0b168b
Antonio Caggiano <info@antoniocaggiano.eu>
2020-10-09 21:29:13 +0000

    Draw a texture
    Add a Texture class, load a png image, improve Vertex with texture
    coordinates attribute, and implement drop to release resources.

--- res/shader/vert.glsl
+++ res/shader/vert.glsl
@@ -2,10 +2,13 @@
 
 layout (location = 0) in vec3 in_pos;
 layout (location = 1) in vec3 in_color;
+layout (location = 2) in vec2 in_tex_coords;
 
 out vec3 color;
+out vec2 tex_coords;
 
 void main() {
     color = in_color;
+    tex_coords = in_tex_coords;
     gl_Position = vec4(in_pos, 1.0);
 }
