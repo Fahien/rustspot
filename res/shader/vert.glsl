7efdbb5ce40236a4b877acd3f6ac53eacf30fc29
Antonio Caggiano <info@antoniocaggiano.eu>
2020-10-10 10:18:26 +0000

    Add a camera
    Use a perspective camera to apply a projection to the rendered scene.
    A node is used as view matrix to move the camera around.

--- res/shader/vert.glsl
+++ res/shader/vert.glsl
@@ -5,6 +5,8 @@ layout (location = 1) in vec3 in_color;
 layout (location = 2) in vec2 in_tex_coords;
 
 layout (location = 0) uniform mat4 model;
+layout (location = 1) uniform mat4 view;
+layout (location = 2) uniform mat4 proj;
 
 out vec3 color;
 out vec2 tex_coords;
@@ -12,5 +14,5 @@ out vec2 tex_coords;
 void main() {
     color = in_color;
     tex_coords = in_tex_coords;
-    gl_Position = model * vec4(in_pos, 1.0);
+    gl_Position = proj * view * model * vec4(in_pos, 1.0);
 }
