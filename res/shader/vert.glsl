c1874736070a23a0f7984d584b2028da7296fc46
Antonio Caggiano <info@antoniocaggiano.eu>
2020-10-17 16:29:01 +0200

    Get uniform locations by their names
    Not all GLSL versions support user defined layout locations for
    uniforms, therefore it is necessary to get the location by querying
    shader programs.

--- res/shader/vert.glsl
+++ res/shader/vert.glsl
@@ -4,9 +4,9 @@ layout (location = 0) in vec3 in_pos;
 layout (location = 1) in vec3 in_color;
 layout (location = 2) in vec2 in_tex_coords;
 
-layout (location = 0) uniform mat4 model;
-layout (location = 1) uniform mat4 view;
-layout (location = 2) uniform mat4 proj;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
 
 out vec3 color;
 out vec2 tex_coords;
