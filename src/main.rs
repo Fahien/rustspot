0d7d38f516ebe5e40a62ccc87cc23f99517b219a
Antonio Caggiano <info@antoniocaggiano.eu>
2020-10-17 16:33:38 +0200

    Introduce SDL2
    This is going to provide support for most of the aspects of the system
    which are not related to graphics rendering, such as windows, input,
    audio, and whatnot. Also, get rid of go2 and drm-sys dependencies.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ use nalgebra as na;
 use rustspot::{gfx::*, util::*};
 
 fn main() {
-    let mut gfx = Gfx::new();
+    let sdl = sdl2::init().expect("Failed to initialize SDL2");
+    let mut events = sdl.event_pump().expect("Failed to initialize SDL2 events");
+
+    let mut gfx = Gfx::new(&sdl);
     let gl_version = gfx.get_gl_version();
     println!("OpenGL v{}.{}", gl_version.0, gl_version.1);
 
@@ -29,7 +32,7 @@ fn main() {
     let mesh = meshes.push(mesh);
 
     // Use texture as a material for the mesh
-    let texture = get_texture("res/img/assets.png");
+    let texture = get_texture("res/img/lena.png");
 
     let mut nodes = Pack::new();
 
@@ -68,7 +71,15 @@ fn main() {
 
     let root = nodes.push(root);
 
-    loop {
+    'gameloop: loop {
+        // Handle SDL2 events
+        for event in events.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } => break 'gameloop,
+                _ => println!("{:?}", event),
+            }
+        }
+
         // Calculate delta time
         let delta = timer.get_delta();
 
