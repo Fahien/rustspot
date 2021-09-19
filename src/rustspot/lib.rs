cf08cd75d64126c5053fc413276d31887af58380
Antonio Caggiano <info@antoniocaggiano.eu>
2021-09-19 13:50:55 +0200

    Spot structure
    Refactor demos by introducing a Spot structure which initializes all
    required subsystems such as SDL and OpenGL.

--- src/rustspot/lib.rs
+++ src/rustspot/lib.rs
@@ -9,3 +9,22 @@ pub use gfx::*;
 
 pub mod util;
 pub use util::*;
+
+pub struct Spot {
+    pub sdl: sdl2::Sdl,
+    pub events: sdl2::EventPump,
+    pub gfx: Gfx,
+}
+
+impl Spot {
+    pub fn new() -> Self {
+        let sdl = sdl2::init().expect("Failed to initialize SDL2");
+        let events = sdl.event_pump().expect("Failed to initialize SDL2 events");
+
+        let gfx = Gfx::new(&sdl);
+        let gl_version = gfx.get_gl_version();
+        println!("OpenGL v{}.{}", gl_version.0, gl_version.1);
+
+        Spot { sdl, events, gfx }
+    }
+}
