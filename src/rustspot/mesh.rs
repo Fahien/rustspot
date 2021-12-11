86fdb630d90068ecb096215db70b54e5f1236907
Antonio Caggiano <info@antoniocaggiano.eu>
2021-12-11 12:51:04 +0100

    Normal mapping
    Add tangent and bi-tangent vectors to the Vertex structure which could
    be used for transforming tangent-space normal perturbations in shaders.

--- src/rustspot/mesh.rs
+++ src/rustspot/mesh.rs
@@ -4,6 +4,8 @@
 
 use crate::*;
 
+use nalgebra as na;
+
 pub struct MeshRes {
     pub vbo: Vbo,
     pub ebo: Ebo,
@@ -27,6 +29,7 @@ impl MeshRes {
         res.ebo.upload(&indices);
 
         let stride = std::mem::size_of::<Vertex>() as i32;
+        let f32size = std::mem::size_of::<f32>();
 
         // These should follow Vao, Vbo, Ebo
         unsafe {
@@ -35,37 +38,25 @@ impl MeshRes {
             gl::EnableVertexAttribArray(0);
 
             // Color
-            gl::VertexAttribPointer(
-                1,
-                3,
-                gl::FLOAT,
-                gl::FALSE,
-                stride,
-                (3 * std::mem::size_of::<f32>()) as _,
-            );
+            gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, stride, (3 * f32size) as _);
             gl::EnableVertexAttribArray(1);
 
             // Texture coordinates
-            gl::VertexAttribPointer(
-                2,
-                2,
-                gl::FLOAT,
-                gl::FALSE,
-                stride,
-                (6 * std::mem::size_of::<f32>()) as _,
-            );
+            gl::VertexAttribPointer(2, 2, gl::FLOAT, gl::FALSE, stride, (6 * f32size) as _);
             gl::EnableVertexAttribArray(2);
 
             // Normal
-            gl::VertexAttribPointer(
-                3,
-                3,
-                gl::FLOAT,
-                gl::TRUE,
-                stride,
-                (8 * std::mem::size_of::<f32>()) as _,
-            );
+            gl::VertexAttribPointer(3, 3, gl::FLOAT, gl::TRUE, stride, (8 * f32size) as _);
             gl::EnableVertexAttribArray(3);
+
+            // TODO enable only when normal map is available?
+            // Tangent
+            gl::VertexAttribPointer(4, 3, gl::FLOAT, gl::TRUE, stride, (11 * f32size) as _);
+            gl::EnableVertexAttribArray(4);
+
+            // Bitangent
+            gl::VertexAttribPointer(5, 3, gl::FLOAT, gl::TRUE, stride, (14 * f32size) as _);
+            gl::EnableVertexAttribArray(5);
         }
 
         res
@@ -160,26 +151,19 @@ impl Primitive {
 
     /// Returns a new unit triangle primitive
     pub fn triangle(material: Handle<Material>) -> Self {
-        let vertices = vec![
-            Vertex {
-                position: [-1.0, 0.0, 0.0],
-                color: [1.0, 1.0, 1.0],
-                tex_coords: [0.0, 0.0],
-                normal: [-0.125, 0.0, 1.0],
-            },
-            Vertex {
-                position: [1.0, 0.0, 0.0],
-                color: [1.0, 1.0, 1.0],
-                tex_coords: [1.0, 0.0],
-                normal: [0.125, 0.0, 1.0],
-            },
-            Vertex {
-                position: [0.0, 1.0, 0.0],
-                color: [1.0, 1.0, 1.0],
-                tex_coords: [0.5, 1.0],
-                normal: [0.0, 0.125, 1.0],
-            },
-        ];
+        let mut vertices = vec![Vertex::new(); 3];
+
+        vertices[0].position = [-1.0, 0.0, 0.0];
+        vertices[0].tex_coords = [0.0, 0.0];
+        vertices[0].normal = na::Vector3::new(-0.125, 0.0, 1.0);
+
+        vertices[1].position = [1.0, 0.0, 0.0];
+        vertices[0].tex_coords = [1.0, 0.0];
+        vertices[0].normal = na::Vector3::new(0.125, 0.0, 1.0);
+
+        vertices[2].position = [0.0, 1.0, 0.0];
+        vertices[0].tex_coords = [0.5, 1.0];
+        vertices[0].normal = na::Vector3::new(0.0, 0.125, 1.0);
 
         let indices = vec![0, 1, 2];
 
@@ -192,32 +176,20 @@ impl Primitive {
 
     /// Returns a new primitive quad with side length 1 centered at the origin
     pub fn quad(material: Handle<Material>) -> Self {
-        let vertices = vec![
-            Vertex {
-                position: [-0.5, -0.5, 0.0],
-                color: [1.0, 1.0, 1.0],
-                tex_coords: [0.0, 0.0],
-                normal: [0.0, 0.0, 1.0],
-            },
-            Vertex {
-                position: [0.5, -0.5, 0.0],
-                color: [1.0, 1.0, 1.0],
-                tex_coords: [1.0, 0.0],
-                normal: [0.0, 0.0, 1.0],
-            },
-            Vertex {
-                position: [0.5, 0.5, 0.0],
-                color: [1.0, 1.0, 1.0],
-                tex_coords: [1.0, 1.0],
-                normal: [0.0, 0.0, 1.0],
-            },
-            Vertex {
-                position: [-0.5, 0.5, 0.0],
-                color: [1.0, 1.0, 1.0],
-                tex_coords: [0.0, 1.0],
-                normal: [0.0, 0.0, 1.0],
-            },
-        ];
+        let mut vertices = vec![Vertex::new(); 4];
+
+        vertices[0].position = [-0.5, -0.5, 0.0];
+        vertices[0].tex_coords = [0.0, 0.0];
+
+        vertices[1].position = [0.5, -0.5, 0.0];
+        vertices[1].tex_coords = [1.0, 0.0];
+
+        vertices[2].position = [0.5, 0.5, 0.0];
+        vertices[2].tex_coords = [1.0, 1.0];
+
+        vertices[3].position = [-0.5, 0.5, 0.0];
+        vertices[3].tex_coords = [0.0, 1.0];
+
         let indices = vec![0, 1, 2, 2, 3, 0];
 
         Self::builder()
@@ -235,85 +207,85 @@ impl Primitive {
         // Front
         vertices[0].position = [-0.5, -0.5, 0.5];
         vertices[0].tex_coords = [1.0 / tex_width, 1.0 / tex_height];
-        vertices[0].normal = [0.0, 0.0, 1.0];
+        vertices[0].normal = na::Vector3::new(0.0, 0.0, 1.0);
         vertices[1].position = [0.5, -0.5, 0.5];
         vertices[1].tex_coords = [2.0 / tex_width, 1.0 / tex_height];
-        vertices[1].normal = [0.0, 0.0, 1.0];
+        vertices[1].normal = na::Vector3::new(0.0, 0.0, 1.0);
         vertices[2].position = [0.5, 0.5, 0.5];
         vertices[2].tex_coords = [2.0 / tex_width, 2.0 / tex_height];
-        vertices[2].normal = [0.0, 0.0, 1.0];
+        vertices[2].normal = na::Vector3::new(0.0, 0.0, 1.0);
         vertices[3].position = [-0.5, 0.5, 0.5];
         vertices[3].tex_coords = [1.0 / tex_width, 2.0 / tex_height];
-        vertices[3].normal = [0.0, 0.0, 1.0];
+        vertices[3].normal = na::Vector3::new(0.0, 0.0, 1.0);
 
         // Right
         vertices[4].position = [0.5, -0.5, 0.5];
-        vertices[4].normal = [1.0, 0.0, 0.0];
+        vertices[4].normal = na::Vector3::new(1.0, 0.0, 0.0);
         vertices[4].tex_coords = [2.0 / tex_width, 1.0 / tex_height];
         vertices[5].position = [0.5, -0.5, -0.5];
-        vertices[5].normal = [1.0, 0.0, 0.0];
+        vertices[5].normal = na::Vector3::new(1.0, 0.0, 0.0);
         vertices[5].tex_coords = [3.0 / tex_width, 1.0 / tex_height];
         vertices[6].position = [0.5, 0.5, -0.5];
-        vertices[6].normal = [1.0, 0.0, 0.0];
+        vertices[6].normal = na::Vector3::new(1.0, 0.0, 0.0);
         vertices[6].tex_coords = [3.0 / tex_width, 2.0 / tex_height];
         vertices[7].position = [0.5, 0.5, 0.5];
-        vertices[7].normal = [1.0, 0.0, 0.0];
+        vertices[7].normal = na::Vector3::new(1.0, 0.0, 0.0);
         vertices[7].tex_coords = [2.0 / tex_width, 2.0 / tex_height];
 
         // Back
         vertices[8].position = [0.5, -0.5, -0.5];
-        vertices[8].normal = [0.0, 0.0, -1.0];
+        vertices[8].normal = na::Vector3::new(0.0, 0.0, -1.0);
         vertices[8].tex_coords = [3.0 / tex_width, 1.0 / tex_height];
         vertices[9].position = [-0.5, -0.5, -0.5];
-        vertices[9].normal = [0.0, 0.0, -1.0];
+        vertices[9].normal = na::Vector3::new(0.0, 0.0, -1.0);
         vertices[9].tex_coords = [4.0 / tex_width, 1.0 / tex_height];
         vertices[10].position = [-0.5, 0.5, -0.5];
-        vertices[10].normal = [0.0, 0.0, -1.0];
+        vertices[10].normal = na::Vector3::new(0.0, 0.0, -1.0);
         vertices[10].tex_coords = [4.0 / tex_width, 2.0 / tex_height];
         vertices[11].position = [0.5, 0.5, -0.5];
-        vertices[11].normal = [0.0, 0.0, -1.0];
+        vertices[11].normal = na::Vector3::new(0.0, 0.0, -1.0);
         vertices[11].tex_coords = [3.0 / tex_width, 2.0 / tex_height];
 
         // Left
         vertices[12].position = [-0.5, -0.5, -0.5];
-        vertices[12].normal = [-1.0, 0.0, 0.0];
+        vertices[12].normal = na::Vector3::new(-1.0, 0.0, 0.0);
         vertices[12].tex_coords = [0.0, 1.0 / tex_height];
         vertices[13].position = [-0.5, -0.5, 0.5];
-        vertices[13].normal = [-1.0, 0.0, 0.0];
+        vertices[13].normal = na::Vector3::new(-1.0, 0.0, 0.0);
         vertices[13].tex_coords = [1.0 / tex_width, 1.0 / tex_height];
         vertices[14].position = [-0.5, 0.5, 0.5];
-        vertices[14].normal = [-1.0, 0.0, 0.0];
+        vertices[14].normal = na::Vector3::new(-1.0, 0.0, 0.0);
         vertices[14].tex_coords = [1.0 / tex_width, 2.0 / tex_height];
         vertices[15].position = [-0.5, 0.5, -0.5];
-        vertices[15].normal = [-1.0, 0.0, 0.0];
+        vertices[15].normal = na::Vector3::new(-1.0, 0.0, 0.0);
         vertices[15].tex_coords = [0.0, 2.0 / tex_height];
 
         // Top
         vertices[16].position = [-0.5, 0.5, 0.5];
-        vertices[16].normal = [0.0, 1.0, 0.0];
+        vertices[16].normal = na::Vector3::new(0.0, 1.0, 0.0);
         vertices[16].tex_coords = [1.0 / tex_width, 2.0 / tex_height];
         vertices[17].position = [0.5, 0.5, 0.5];
-        vertices[17].normal = [0.0, 1.0, 0.0];
+        vertices[17].normal = na::Vector3::new(0.0, 1.0, 0.0);
         vertices[17].tex_coords = [2.0 / tex_width, 2.0 / tex_height];
         vertices[18].position = [0.5, 0.5, -0.5];
-        vertices[18].normal = [0.0, 1.0, 0.0];
+        vertices[18].normal = na::Vector3::new(0.0, 1.0, 0.0);
         vertices[18].tex_coords = [2.0 / tex_width, 3.0 / tex_height];
         vertices[19].position = [-0.5, 0.5, -0.5];
-        vertices[19].normal = [0.0, 1.0, 0.0];
+        vertices[19].normal = na::Vector3::new(0.0, 1.0, 0.0);
         vertices[19].tex_coords = [1.0 / tex_width, 3.0 / tex_height];
 
         // Bottom
         vertices[20].position = [-0.5, -0.5, -0.5];
-        vertices[20].normal = [0.0, -1.0, 0.0];
+        vertices[20].normal = na::Vector3::new(0.0, -1.0, 0.0);
         vertices[20].tex_coords = [1.0 / tex_width, 0.0];
         vertices[21].position = [0.5, -0.5, -0.5];
-        vertices[21].normal = [0.0, -1.0, 0.0];
+        vertices[21].normal = na::Vector3::new(0.0, -1.0, 0.0);
         vertices[21].tex_coords = [2.0 / tex_width, 0.0];
         vertices[22].position = [0.5, -0.5, 0.5];
-        vertices[22].normal = [0.0, -1.0, 0.0];
+        vertices[22].normal = na::Vector3::new(0.0, -1.0, 0.0);
         vertices[22].tex_coords = [2.0 / tex_width, 1.0 / tex_height];
         vertices[23].position = [-0.5, -0.5, 0.5];
-        vertices[23].normal = [0.0, -1.0, 0.0];
+        vertices[23].normal = na::Vector3::new(0.0, -1.0, 0.0);
         vertices[23].tex_coords = [1.0 / tex_width, 1.0 / tex_height];
 
         let indices = vec![
