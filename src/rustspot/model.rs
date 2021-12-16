30a85d4209842adebd0169e241e9acd42635b29f
Antonio Caggiano <info@antoniocaggiano.eu>
2021-12-16 12:47:46 +0100

    Model builder
    Add a builder structure for the Model which is responsible to create a
    model from loading a glTF file. We add also code for loading transform
    matrices and texture coordinates.

--- src/rustspot/model.rs
+++ src/rustspot/model.rs
@@ -2,68 +2,79 @@
 // Author: Antonio Caggiano <info@antoniocaggiano.eu>
 // SPDX-License-Identifier: MIT
 
-use std::{collections::HashMap, convert::TryInto, error::Error, path::Path};
+use std::{
+    collections::HashMap,
+    convert::TryInto,
+    error::Error,
+    path::{Path, PathBuf},
+};
 
 use gltf::Gltf;
+use nalgebra as na;
 use rayon::iter::{ParallelBridge, ParallelIterator};
 
 use super::*;
 
-pub struct Model {
-    pub colors: HashMap<Color, Texture>,
-    pub textures: Pack<Texture>,
-    pub materials: Pack<Material>,
-    pub primitives: Pack<Primitive>,
-    pub meshes: Pack<Mesh>,
-    pub nodes: Pack<Node>,
-    pub directional_lights: Pack<DirectionalLight>,
-    pub point_lights: Pack<PointLight>,
-    pub cameras: Pack<Camera>,
+pub struct ModelBuilder {
+    uri_buffers: Vec<Vec<u8>>,
+    parent_dir: PathBuf,
+    gltf: Gltf,
 }
 
-impl Model {
-    pub fn new() -> Self {
-        Self {
-            colors: HashMap::new(),
-            textures: Pack::new(),
-            materials: Pack::new(),
-            primitives: Pack::new(),
-            meshes: Pack::new(),
-            nodes: Pack::new(),
-            directional_lights: Pack::new(),
-            point_lights: Pack::new(),
-            cameras: Pack::new(),
-        }
+impl ModelBuilder {
+    /// Creates a model loading a GLTF file
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, Box<dyn Error>> {
+        let ret = Self {
+            uri_buffers: vec![],
+            parent_dir: path
+                .as_ref()
+                .parent()
+                .ok_or("Failed to get parent directory")?
+                .into(),
+            gltf: Gltf::open(path)?,
+        };
+        Ok(ret)
     }
 
-    /// Creates a model loading a GLTF file
-    pub fn gltf<P: AsRef<Path>>(path: P) -> Result<Self, Box<dyn Error>> {
-        let mut model = Model::new();
-
-        let gltf = Gltf::open(&path)?;
-
-        // Load URI buffers
-        let mut uri_buffers = HashMap::new();
-        for buffer in gltf.buffers() {
+    fn load_uri_buffers(&mut self) -> Result<(), Box<dyn Error>> {
+        for buffer in self.gltf.buffers() {
             match buffer.source() {
                 gltf::buffer::Source::Uri(uri) => {
-                    let dir = path.as_ref().parent().ok_or("Can't get parent path")?;
-                    let uri = dir.join(uri);
+                    let uri = self.parent_dir.join(uri);
                     let data = std::fs::read(uri)?;
-                    uri_buffers.insert(buffer.index(), data);
+                    assert!(buffer.index() == self.uri_buffers.len());
+                    self.uri_buffers.push(data);
                 }
-                _ => (),
+                _ => unimplemented!(),
             }
         }
+        Ok(())
+    }
 
-        // Load materials
-        for gmaterial in gltf.materials() {
+    pub fn load_materials(&mut self, model: &mut Model) {
+        for gmaterial in self.gltf.materials() {
             let mut material = Material::builder().build();
-            if gmaterial
-                .pbr_metallic_roughness()
-                .base_color_texture()
-                .is_none()
-            {
+
+            let pbr = gmaterial.pbr_metallic_roughness();
+            if let Some(gtexture) = pbr.base_color_texture() {
+                match gtexture.texture().source().source() {
+                    gltf::image::Source::Uri { uri, .. } => {
+                        let uri = self.parent_dir.join(uri);
+                        let texture_handle = if let Some((index, _)) =
+                            model.textures.iter().enumerate().find(|(_, texture)| {
+                                texture.path.is_some()
+                                    && texture.path.as_ref().unwrap() == uri.to_str().unwrap()
+                            }) {
+                            Handle::new(index)
+                        } else {
+                            let texture = Texture::open(uri);
+                            model.textures.push(texture)
+                        };
+                        material.texture = Some(texture_handle);
+                    }
+                    _ => unimplemented!(),
+                }
+            } else {
                 let gcolor = gmaterial.pbr_metallic_roughness().base_color_factor();
                 let color = Color::rgba(
                     (gcolor[0] * 255.0) as u8,
@@ -80,9 +91,76 @@ impl Model {
 
             model.materials.push(material);
         }
+    }
 
-        // Load meshes
-        for gmesh in gltf.meshes() {
+    pub fn build(&mut self) -> Result<Model, Box<dyn Error>> {
+        let mut model = Model::new();
+
+        self.load_uri_buffers()?;
+        self.load_materials(&mut model);
+        self.load_meshes(&mut model)?;
+
+        // Load scene
+        let scene = self.gltf.scenes().next().unwrap();
+        let root = Node::builder()
+            .name("Root".into())
+            .children(
+                scene
+                    .nodes()
+                    .par_bridge()
+                    .map(|gchild| Handle::new(gchild.index() + 1))
+                    .collect(),
+            )
+            .build();
+        // Root is always at index 0
+        model.nodes.push(root);
+
+        // Load nodes
+        for gnode in self.gltf.nodes() {
+            let mut node_builder = Node::builder()
+                .id(gnode.index() as u32 + 1)
+                .name(gnode.name().unwrap_or("Unknown").into())
+                .children(
+                    gnode
+                        .children()
+                        .par_bridge()
+                        .map(|gchild| Handle::new(gchild.index() + 1))
+                        .collect(),
+                );
+
+            let transform = gnode.transform().decomposed();
+
+            let translation = &transform.0;
+            let translation = na::Translation3::new(translation[0], translation[1], translation[2]);
+            node_builder = node_builder.translation(translation);
+
+            // xyzw
+            let rotation = &transform.1;
+            let rotation = na::UnitQuaternion::from_quaternion(na::Quaternion::new(
+                rotation[3],
+                rotation[0],
+                rotation[1],
+                rotation[2],
+            ));
+            node_builder = node_builder.rotation(rotation);
+
+            let scale = &transform.2;
+            let scale = na::Vector3::new(scale[0], scale[1], scale[2]);
+            node_builder = node_builder.scale(scale);
+
+            if let Some(mesh) = gnode.mesh() {
+                node_builder = node_builder.mesh(Handle::new(mesh.index()));
+            }
+
+            let node = node_builder.build();
+            model.nodes.push(node);
+        }
+
+        Ok(model)
+    }
+
+    fn load_meshes(&self, model: &mut Model) -> Result<(), Box<dyn Error>> {
+        for gmesh in self.gltf.meshes() {
             let mut primitive_handles = vec![];
 
             for gprimitive in gmesh.primitives() {
@@ -92,78 +170,17 @@ impl Model {
                 assert!(mode == gltf::mesh::Mode::Triangles);
 
                 for (semantic, accessor) in gprimitive.attributes() {
-                    if semantic == gltf::mesh::Semantic::Positions {
-                        let data_type = accessor.data_type();
-                        assert!(data_type == gltf::accessor::DataType::F32);
-                        let count = accessor.count();
-                        assert!(count == 24);
-                        let dimensions = accessor.dimensions();
-                        assert!(dimensions == gltf::accessor::Dimensions::Vec3);
-
-                        let view = accessor.view().unwrap();
-                        let buffer = view.buffer();
-                        match buffer.source() {
-                            gltf::buffer::Source::Bin => unimplemented!(),
-                            _ => (),
-                        };
-
-                        let target = view.target().unwrap_or(gltf::buffer::Target::ArrayBuffer);
-                        assert!(target == gltf::buffer::Target::ArrayBuffer);
-
-                        let data = &uri_buffers[&buffer.index()];
-
-                        for i in 0..count {
-                            let offset = accessor.offset()
-                                + view.offset()
-                                + i * view.stride().unwrap_or_default();
-                            assert!(offset < data.len());
-                            let d = &data[offset];
-                            let position = unsafe {
-                                std::slice::from_raw_parts::<f32>(d as *const u8 as _, 3)
-                            };
-                            println!("Read position {:?}", position);
-
-                            if vertices.len() <= i {
-                                vertices.push(Vertex::new())
-                            }
-                            vertices[i].position = position.try_into()?;
+                    match semantic {
+                        gltf::mesh::Semantic::Positions => {
+                            self.load_positions(&mut vertices, &accessor)?
                         }
-                    } else if semantic == gltf::mesh::Semantic::Normals {
-                        let data_type = accessor.data_type();
-                        assert!(data_type == gltf::accessor::DataType::F32);
-                        let count = accessor.count();
-                        assert!(count == 24);
-                        let dimensions = accessor.dimensions();
-                        assert!(dimensions == gltf::accessor::Dimensions::Vec3);
-
-                        let view = accessor.view().unwrap();
-                        let buffer = view.buffer();
-                        match buffer.source() {
-                            gltf::buffer::Source::Bin => unimplemented!(),
-                            _ => (),
-                        };
-
-                        let target = view.target().unwrap_or(gltf::buffer::Target::ArrayBuffer);
-                        assert!(target == gltf::buffer::Target::ArrayBuffer);
-
-                        let data = &uri_buffers[&buffer.index()];
-
-                        for i in 0..count {
-                            let offset = accessor.offset()
-                                + view.offset()
-                                + i * view.stride().unwrap_or_default();
-                            assert!(offset < data.len());
-                            let d = &data[offset];
-                            let normal = unsafe {
-                                std::slice::from_raw_parts::<f32>(d as *const u8 as _, 3)
-                            };
-                            println!("Read normal {:?}", normal);
-
-                            if vertices.len() <= i {
-                                vertices.push(Vertex::new())
-                            }
-                            vertices[i].normal = normal.try_into()?;
+                        gltf::mesh::Semantic::Normals => {
+                            self.load_normals(&mut vertices, &accessor)?
                         }
+                        gltf::mesh::Semantic::TexCoords(_) => {
+                            self.load_tex_coords(&mut vertices, &accessor)?
+                        }
+                        _ => unimplemented!(),
                     }
                 }
 
@@ -172,7 +189,7 @@ impl Model {
                     assert!(accessor.data_type() == gltf::accessor::DataType::U16);
                     let view = accessor.view().unwrap();
                     let offset = accessor.offset() + view.offset();
-                    let data = &uri_buffers[&view.buffer().index()];
+                    let data = &self.uri_buffers[view.buffer().index()];
                     assert!(offset < data.len());
                     let d = &data[offset];
                     indices = unsafe {
@@ -192,40 +209,155 @@ impl Model {
             model.meshes.push(mesh);
         }
 
-        // Load scene
-        let scene = gltf.scenes().next().unwrap();
-        let root = Node::builder()
-            .name("Root".into())
-            .children(
-                scene
-                    .nodes()
-                    .par_bridge()
-                    .map(|gchild| Handle::new(gchild.index() + 1))
-                    .collect(),
-            )
-            .build();
-        model.nodes.push(root);
+        Ok(())
+    }
 
-        for gnode in gltf.nodes() {
-            let mut node_builder = Node::builder()
-                .id(gnode.index() as u32 + 1)
-                .name(gnode.name().unwrap_or("Unknown").into())
-                .children(
-                    gnode
-                        .children()
-                        .par_bridge()
-                        .map(|gchild| Handle::new(gchild.index() + 1))
-                        .collect(),
-                );
+    fn load_positions(
+        &self,
+        vertices: &mut Vec<Vertex>,
+        accessor: &gltf::Accessor,
+    ) -> Result<(), Box<dyn Error>> {
+        let data_type = accessor.data_type();
+        assert!(data_type == gltf::accessor::DataType::F32);
+        let count = accessor.count();
+        let dimensions = accessor.dimensions();
+        assert!(dimensions == gltf::accessor::Dimensions::Vec3);
 
-            if let Some(mesh) = gnode.mesh() {
-                node_builder = node_builder.mesh(Handle::new(mesh.index()));
+        let view = accessor.view().unwrap();
+        let buffer = view.buffer();
+        match buffer.source() {
+            gltf::buffer::Source::Bin => unimplemented!(),
+            _ => (),
+        };
+
+        let target = view.target().unwrap_or(gltf::buffer::Target::ArrayBuffer);
+        assert!(target == gltf::buffer::Target::ArrayBuffer);
+
+        let data = &self.uri_buffers[buffer.index()];
+
+        for i in 0..count {
+            let offset = accessor.offset() + view.offset() + i * view.stride().unwrap_or_default();
+            assert!(offset < data.len());
+            let d = &data[offset];
+            let position = unsafe { std::slice::from_raw_parts::<f32>(d as *const u8 as _, 3) };
+            println!("Read position {:?}", position);
+
+            if vertices.len() <= i {
+                vertices.push(Vertex::new())
             }
-
-            let node = node_builder.build();
-            model.nodes.push(node);
+            vertices[i].position = position.try_into()?;
         }
 
-        Ok(model)
+        Ok(())
+    }
+
+    fn load_normals(
+        &self,
+        vertices: &mut Vec<Vertex>,
+        accessor: &gltf::Accessor,
+    ) -> Result<(), Box<dyn Error>> {
+        let data_type = accessor.data_type();
+        assert!(data_type == gltf::accessor::DataType::F32);
+        let count = accessor.count();
+        let dimensions = accessor.dimensions();
+        assert!(dimensions == gltf::accessor::Dimensions::Vec3);
+
+        let view = accessor.view().unwrap();
+        let buffer = view.buffer();
+        match buffer.source() {
+            gltf::buffer::Source::Bin => unimplemented!(),
+            _ => (),
+        };
+
+        let target = view.target().unwrap_or(gltf::buffer::Target::ArrayBuffer);
+        assert!(target == gltf::buffer::Target::ArrayBuffer);
+
+        let data = &self.uri_buffers[buffer.index()];
+
+        for i in 0..count {
+            let offset = accessor.offset() + view.offset() + i * view.stride().unwrap_or_default();
+            assert!(offset < data.len());
+            let d = &data[offset];
+            let normal = unsafe { std::slice::from_raw_parts::<f32>(d as *const u8 as _, 3) };
+            println!("Read normal {:?}", normal);
+
+            if vertices.len() <= i {
+                vertices.push(Vertex::new())
+            }
+            vertices[i].normal = normal.try_into()?;
+        }
+
+        Ok(())
+    }
+
+    fn load_tex_coords(
+        &self,
+        vertices: &mut Vec<Vertex>,
+        accessor: &gltf::Accessor,
+    ) -> Result<(), Box<dyn Error>> {
+        let data_type = accessor.data_type();
+        assert!(data_type == gltf::accessor::DataType::F32);
+        let count = accessor.count();
+        let dimensions = accessor.dimensions();
+        assert!(dimensions == gltf::accessor::Dimensions::Vec2);
+
+        let view = accessor.view().unwrap();
+        let buffer = view.buffer();
+        match buffer.source() {
+            gltf::buffer::Source::Bin => unimplemented!(),
+            _ => (),
+        };
+
+        let target = view.target().unwrap_or(gltf::buffer::Target::ArrayBuffer);
+        assert!(target == gltf::buffer::Target::ArrayBuffer);
+
+        let data = &self.uri_buffers[buffer.index()];
+
+        for i in 0..count {
+            let offset = accessor.offset() + view.offset() + i * view.stride().unwrap_or_default();
+            assert!(offset < data.len());
+            let d = &data[offset];
+            let tex_coords = unsafe { std::slice::from_raw_parts::<f32>(d as *const u8 as _, 2) };
+            println!("Read tex_coords {:?}", tex_coords);
+
+            if vertices.len() <= i {
+                vertices.push(Vertex::new())
+            }
+            vertices[i].tex_coords = tex_coords.try_into()?;
+        }
+
+        Ok(())
+    }
+}
+
+pub struct Model {
+    pub colors: HashMap<Color, Texture>,
+    pub textures: Pack<Texture>,
+    pub materials: Pack<Material>,
+    pub primitives: Pack<Primitive>,
+    pub meshes: Pack<Mesh>,
+    pub nodes: Pack<Node>,
+    pub directional_lights: Pack<DirectionalLight>,
+    pub point_lights: Pack<PointLight>,
+    pub cameras: Pack<Camera>,
+}
+
+impl Model {
+    pub fn builder<P: AsRef<Path>>(path: P) -> Result<ModelBuilder, Box<dyn Error>> {
+        ModelBuilder::new(path)
+    }
+
+    pub fn new() -> Self {
+        Self {
+            colors: HashMap::new(),
+            textures: Pack::new(),
+            materials: Pack::new(),
+            primitives: Pack::new(),
+            meshes: Pack::new(),
+            nodes: Pack::new(),
+            directional_lights: Pack::new(),
+            point_lights: Pack::new(),
+            cameras: Pack::new(),
+        }
     }
 }
